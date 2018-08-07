d2733760904c06da0729d208e32e44de632fcede
amontess <amontess@LES005469.corp.capgemini.com>
2018-08-07 09:21:10 +0200

    Clase Estudiante creada el Martes.

---
+++ GitHubDemo/src/main/java/com/ainhoa/modelo/Estudiante.java
@@ -0,0 +1,20 @@
+package com.ainhoa.modelo;
+
+public class Estudiante {
+
+	String nombre;
+
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+}
